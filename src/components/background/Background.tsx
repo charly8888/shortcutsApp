1a012a8687b939c4c4605823ca78c5e1e3014d3f
German Hornus <hornusgerman@gmail.com>
2022-06-14 22:39:29 -0300

    add comment of beta

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,6 +1,10 @@
 import styles from './Background.module.scss'
 const Background = () => {
-  return <div className={styles.background}></div>
+  return (
+    <div className={styles.background}>
+      <h5>Beta version, don't save anything that you don't permit it loss </h5>
+    </div>
+  )
 }
 
 export default Background
